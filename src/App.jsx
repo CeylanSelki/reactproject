dd9c901bbfcfc1262f1a369786594a9d65d79666
[Ceylan] <[ceylancengiz1@gmail.com]>
2023-10-11 21:24:21 +0300

    postlar eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
 import Nav from './components/Nav'
-import Header from './components/Header'
 import Footer from './components/Footer'
+import Posts from './components/Posts'
+import PostDetail from './components/PostDetail'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <Posts />
+      },
+      {
+        path: '/:slug',
+        element: <PostDetail />
+      }
+    ]
+  }
+]);
 
+function Root() {
   return (
     <>
       <Nav />
-      <Header />
+      <Outlet />
+      
       <Footer />
     </>
   )
+}
+
+function App() {
+  return (<RouterProvider router={router} />)
 }
 
 export default App
---
+++ src/components/Posts.jsx
@@ -0,0 +1,41 @@
+import Header from './Header'
+import { NavLink } from 'react-router-dom'
+
+function PostItem({ title, subTitle, slug, author, authorSlug, publishDate }) {
+    return (
+        <>
+            {/* <!-- Post preview--> */}
+            <div className="post-preview">
+                <NavLink to={slug}>
+                    <h2 className="post-title">{title}</h2>
+                    <h3 className="post-subtitle">{subTitle}</h3>
+                </NavLink>
+                <p className="post-meta">
+                    <a href={authorSlug}>{author}</a> tarafından {publishDate} tarihinde paylaşıldı
+                </p>
+            </div>
+            {/* <!-- Divider--> */}
+            <hr className="my-4" />
+        </>
+    )
+}
+
+export default function Posts() {
+    return (
+        <>
+            <Header />
+            <div className="container px-4 px-lg-5">
+                <div className="row gx-4 gx-lg-5 justify-content-center">
+                    <div className="col-md-10 col-lg-8 col-xl-7">
+
+                        <PostItem title={'Merhaba Dünya'} subTitle={'Bu benim ilk yazım'} slug={'/merhaba-dunya'} publishDate={'24 Eylül 2023'} author={'Orhan Ekici'} authorSlug={'/yazar/orhan-ekici'} />
+                        
+                        {/* <!-- Pager--> */}
+                        {/* <div className="d-flex justify-content-end mb-4"><a className="btn btn-primary text-uppercase" href="#!">Older Posts →</a></div> */}
+                    </div>
+                </div>
+            </div>
+        </>
+        
+    )
+}
