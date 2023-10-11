3f631d7531abd9cf9c03e774c12174f30b2a1bc4
[Ceylan] <[ceylancengiz1@gmail.com]>
2023-10-11 20:14:23 +0300

    header url düzenlendi yeniden

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,15 @@
-export default function Header(title= 'Blog', subTitle= 'Basit Blog', imgUrl= 'assets/img/home-bg.jpg') {
+export default function Header({ title = 'Blog', subTitle = 'Basit Blog', imgUrl = 'assets/img/home-bg.jpg' }) {
     return (
-        <header className="masthead" style={{ backgroundImage: imgUrl}}>
-            <div className="container position-relative px-4 px-lg-5">
-                <div className="row gx-4 gx-lg-5 justify-content-center">
-                    <div className="col-md-10 col-lg-8 col-xl-7">
-                        <div className="site-heading">
-                            <h1>{title}</h1>
-                            <span className="subheading">{subTitle}</span>
-                        </div>
-                    </div>
+    <header className="masthead" style={{ backgroundImage: `url("${imgUrl}")` }}>
+    <div className="container position-relative px-4 px-lg-5">
+        <div className="row gx-4 gx-lg-5 justify-content-center">
+            <div className="col-md-10 col-lg-8 col-xl-7">
+                <div className="site-heading">
+                    <h1>{title}</h1>
+                    <span className="subheading">{subTitle}</span>
                 </div>
             </div>
-        </header>
-
-    )
+        </div>
+    </div>
+</header>)
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -17,6 +17,5 @@ export default function Nav() {
                 </div>
             </div>
         </nav>
-
     )
 }
